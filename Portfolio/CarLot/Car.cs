8ce4e900c827869564d23f8ab0ac2234d6e37ca4
MarlyssaB <marlyssa.boucher@gmail.com>
2019-07-27 23:38:49 -0500

    Adding carlot files

---
+++ Portfolio/CarLot/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLot
+{
+    public class Car : Vehicle
+    {
+        public string Type;
+        public byte Doors;
+        public Car(string type, byte doors, int licenseNumber, string make, string model, double price) : base(licenseNumber, make, model, price)
+        {
+            Type = type;
+            Doors = doors;
+        }
+        public override string VehicleInfo() { return string.Format("Car: License {0}, {1}-door {2} {3} {4}, ${5}", LicenseNumber, Doors, Make, Model, Type, Price); }
+
+    }
+}
