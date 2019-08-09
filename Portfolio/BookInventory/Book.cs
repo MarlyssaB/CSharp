5bfe5bb3abe4913396261a265d178a3596600e66
MarlyssaB <marlyssa.boucher@gmail.com>
2019-08-08 20:18:56 -0500

    adding book database files

---
+++ Portfolio/BookInventory/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookInventory
+{
+    class Book
+    {
+        public int ID { get; private set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public Book (string Title, string Author)
+        {
+            this.Title = Title;
+            this.Author = Author;
+        }
+    }
+}
---
+++ Portfolio/BookInventory/BooksContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace BookInventory
+{
+    class BooksContext : DbContext
+    {
+        public DbSet<Book> books { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            DirectoryInfo ExecutionDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            DirectoryInfo ProjectBase = ExecutionDirectory.Parent.Parent.Parent;
+
+            String DatabaseFile = Path.Combine(ProjectBase.FullName, "books.db");
+
+            optionsBuilder.UseSqlite("Data Source = " + DatabaseFile);
+        }
+
+
+    }
+    
+    
+}
